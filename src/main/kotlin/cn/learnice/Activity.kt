aab470ee47bc01f9c62e4b43c020b5583dd8e649
XuebinHe <learnice.he@gmail.com>
2021-04-06 16:52:55 +0800

    mod:open_source

--- src/main/kotlin/cn/learnice/Activity.kt
+++ src/main/kotlin/cn/learnice/Activity.kt
@@ -1,19 +1,24 @@
 package cn.learnice
 
 import java.awt.Color
+import java.awt.Desktop
 import java.awt.Dimension
 import java.awt.FlowLayout
+import java.net.URI
 import javax.swing.*
 
 object Activity {
     private var jStatement: JLabel
+    private var jOpenSource: JButton
     private val jButtonCopy: JButton
     private const val TITLE = "Acquire Active Code"
     private const val STATEMENT = "<html><body>" +
             "<p align=\"center\">声明：本软件仅仅用于开发学习，请勿用作商业用途</p>" +
             "<p align=\"center\">Disclaimer: This software is only for development learning, please do not use for commercial purposes</p>" +
             "<body></html>"
-
+    private const val GITHUB = "https://github.com/learnice/AquireActiveCode"
+    private val githubUrl = URI(GITHUB)
+    private const val OPEN_SOURCE = "Open Source"
     private const val COPY_ACTION = "复制激活码"
 
     init {
@@ -24,6 +29,15 @@ object Activity {
         jStatement.preferredSize = jStatementD
         jStatement.horizontalAlignment = SwingConstants.CENTER
         jStatement.foreground = Color.red
+        jOpenSource = JButton()
+        jOpenSource.text = OPEN_SOURCE
+        jOpenSource.preferredSize = Dimension(100, 50)
+        jOpenSource.addActionListener {
+            try {
+                Desktop.getDesktop().browse(githubUrl)
+            } catch (e: Exception) {
+            }
+        }
         jButtonCopy = JButton()
         jButtonCopy.text = COPY_ACTION
         val dimension = Dimension(100, 50)
@@ -34,6 +48,7 @@ object Activity {
         val panel1 = JPanel(FlowLayout(FlowLayout.CENTER))
         panel1.add(jStatement)
         panel1.add(jButtonCopy)
+        panel1.add(jOpenSource)
         jFrame.isResizable = false
         jFrame.setSize(600, 200)
         jFrame.setLocationRelativeTo(null)
